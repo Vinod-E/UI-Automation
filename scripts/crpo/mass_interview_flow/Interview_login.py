2ec1c8e0175ed78e7d8895f6eb08a46710cf07e9
Vinod Kumar Eraganaboina <Vinod-E>
2020-12-17 19:04:58 +0530

    Mass interview Flow

--- scripts/crpo/mass_interview_flow/Interview_login.py
+++ scripts/crpo/mass_interview_flow/Interview_login.py
@@ -1,7 +1,6 @@
 import time
 import page_elements
 from logger_settings import ui_logger
-from scripts.crpo.common import button_click
 from scripts.crpo.mass_interview_flow import manage_candidate
 
 
@@ -9,11 +8,22 @@ class InterviewLogin(manage_candidate.ManageCandidate):
     def __init__(self):
         super(InterviewLogin, self).__init__()
 
+        self.int_name_verification = ''
+
+        self.ui_int1_login_m = ''
+        self.ui_event_tab_m_2 = ''
+        self.ui_advance_search_m_2 = ''
+        self.ui_event_details_m_2 = ''
+        self.ui_event_validation_m_3 = ''
+        self.ui_floating_action_m_4 = ''
+        self.ui_view_interview_panel_action_m = ''
+        self.int1_panel_validation_m = ''
+
     def int1_login(self):
         try:
             time.sleep(0.5)
             self.crpo_logout()
-            self.login('InterviewerTWO', self.xl_int1_name, self.xl_int1_pwd)
+            self.login('InterviewerTWO', self.xl_int1_user, self.xl_int1_pwd)
             # -------------------------------- Submit feedback Process -------------------------------------------------
             self.advance_search(page_elements.tabs['event_tab'])
             self.name_search(self.event_sprint_version_m, 'Event')
@@ -22,16 +32,31 @@ class InterviewLogin(manage_candidate.ManageCandidate):
             self.actions_dropdown()
             self.floating_action('view_interview_panel')
             time.sleep(0.5)
+
+            # -------------------- output report values ----------------
+            if self.event_validation_check == 'True':
+                print('**-------->>> Landed into event screen successfully')
+                self.ui_int1_login_m = 'Pass'
+                self.ui_event_tab_m_2 = 'Pass'
+                self.ui_advance_search_m_2 = 'Pass'
+                self.ui_event_details_m_2 = 'Pass'
+                self.ui_event_validation_m_3 = 'Pass'
+                self.ui_floating_action_m_4 = 'Pass'
+                self.ui_view_interview_panel_action_m = 'Pass'
+
+            self.interview_panel_validation()
+            if self.int_name_verification == 'Welcome {}'.format(self.interviewer_1_name):
+                self.int1_panel_validation_m = 'Pass'
+
         except Exception as error:
             ui_logger.error(error)
 
+    def interview_panel_validation(self):
+        try:
+            self.web_element_text_xpath(page_elements.mass_interview['interviewer_name'])
+            self.int_name_verification = self.text_value
+            if self.int_name_verification == 'Welcome {}'.format(self.interviewer_1_name):
+                print('**-------->>> Entered into Interview panel lobby')
 
-Object = InterviewLogin()
-Object.crpo_login()
-Object.event_search()
-Object.slot_config()
-Object.candidate_login_page()
-Object.create_room()
-Object.manage_candidate()
-Object.tag_candidate_to_room()
-Object.int1_login()
+        except Exception as error:
+            ui_logger.error(error)
---
+++ scripts/crpo/mass_interview_flow/provide_mass_feedback.py
@@ -0,0 +1,87 @@
+import time
+import page_elements
+from logger_settings import ui_logger
+from scripts.crpo.common import button_click
+from scripts.crpo.mass_interview_flow import select_candidate
+
+
+class MassFeedback(select_candidate.SelectCandidate):
+    def __init__(self):
+        super(MassFeedback, self).__init__()
+        self.message_m = ''
+
+        self.ui_provide_feedback_action_m = ''
+        self.ui_p_f_screen_validate = ''
+        self.ui_decision_select = ''
+        self.ui_p_f_submit_button = ''
+        self.ui_p_f_submitted = ''
+        self.ui_view_profile_action = ''
+        self.ui_profile_screen_validate = ''
+        self.ui_candidate_status_m = ''
+        self.ui_interview_finish_button = ''
+        self.ui_finished_interview = ''
+        self.ui_next_candidate_screen_validate = ''
+
+    def mass_feedback(self):
+        try:
+            button_click.all_buttons(self, 'Provide Feedback')
+            time.sleep(3)
+            self.driver.switch_to.window(self.driver.window_handles[1])
+            self.web_element_click_xpath(page_elements.interview['shortlist'])
+            self.web_element_send_keys_xpath(page_elements.interview['rating_1'], 'Develop')
+            self.web_element_send_keys_xpath(page_elements.interview['comment_1'], self.xl_comment_m)
+            self.web_element_send_keys_xpath(page_elements.interview['rating_2'], 'Develop')
+            self.web_element_send_keys_xpath(page_elements.interview['comment_2'], self.xl_comment_m)
+            self.web_element_send_keys_xpath(page_elements.mass_interview['overall_comment'], self.xl_comment_m)
+
+            # ------ Submitted
+            time.sleep(1)
+            self.driver.execute_script("window.scrollTo(0,200);")
+            button_click.all_buttons(self, 'Submit Feedback')
+            time.sleep(0.3)
+            button_click.all_buttons(self, 'Agree and Submit')
+            button_click.all_buttons(self, 'Agree and Submit')
+            time.sleep(7)
+            self.driver.switch_to.window(self.driver.window_handles[0])
+
+        except Exception as error:
+            ui_logger.error(error)
+
+    def candidate_feedback_status(self):
+        try:
+            button_click.all_buttons(self, 'View Profile')
+            time.sleep(1)
+            self.driver.switch_to.window(self.driver.window_handles[1])
+            self.applicant_current_status_validation('Shortlisted')
+            time.sleep(2)
+            self.driver.close()
+            self.driver.switch_to.window(self.driver.window_handles[0])
+
+            # ------- Interview Finished ------------------
+            button_click.button(self, 'Interview is Finished')
+            time.sleep(0.6)
+            button_click.button(self, 'Interview is Finished')
+            time.sleep(1.5)
+            self.validation_next_candidate_screen()
+
+        except Exception as error:
+            ui_logger.error(error)
+
+    def validation_next_candidate_screen(self):
+        try:
+            self.web_element_text_xpath(page_elements.mass_interview['message'])
+            self.message_m = self.text_value
+            if self.message_m.strip() == 'There are no candidates in the queue for interview.':
+                self.ui_provide_feedback_action_m = 'Pass'
+                self.ui_p_f_screen_validate = 'Pass'
+                self.ui_decision_select = 'Pass'
+                self.ui_p_f_submit_button = 'Pass'
+                self.ui_p_f_submitted = 'Pass'
+                self.ui_view_profile_action = 'Pass'
+                self.ui_profile_screen_validate = 'Pass'
+                self.ui_candidate_status_m = 'Pass'
+                self.ui_interview_finish_button = 'Pass'
+                self.ui_finished_interview = 'Pass'
+                self.ui_next_candidate_screen_validate = 'Pass'
+        except Exception as error:
+            ui_logger.error(error)
